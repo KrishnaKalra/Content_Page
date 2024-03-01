85f60d437c45035a173a6a5c864055e138c41059
KrishnaKalra <148621724+KrishnaKalra@users.noreply.github.com>
2024-03-01 06:55:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function getValue(){
     var Name=document.querySelector(".name").value;
     var Date=document.querySelector(".date").value;
     var Time=document.querySelector(".time").value;
-    alert(Name);
-    alert(Date);
-    alert(Time);
-    var lik
+    Date=Date.replace("-","");
+    Date=Date.replace("-","");
+    Time=Time.replace(":","");
+    var linkp1="https://calendar.google.com/calendar/render?action=TEMPLATE&text="+Name+"&details=Event description text&dates="+Date+"T"+Time+"00/"+Date+"T"+Time+"00";
+    window.open(linkp1);
 }
