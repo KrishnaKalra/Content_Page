61594180a6a90035e8d4abc408f1d6222f4fb516
KrishnaKalra <148621724+KrishnaKalra@users.noreply.github.com>
2024-03-01 06:52:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function getValue(){
+    var Name=document.querySelector(".name").value;
+    var Date=document.querySelector(".date").value;
+    var Time=document.querySelector(".time").value;
+    alert(Name);
+    alert(Date);
+    alert(Time);
+    var lik
+}
